766a86b604d17ac45a82a82efd10c01e35665f04
root <root@carbon.host.funtoo.org>
2016-05-03 09:02:41 -0700

    Nginx

--- cert/renew.sh
+++ cert/renew.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-WEBROOT=/home/worker/gov2017
+WEBROOT=/home/worker/gov2017/cert/
 DOMAIN=gatheringourvoices.bcaafc.com,gov.hoverbear.org
 EMAIL=andrew@hoverbear.org
 KEY_SIZE=4096
 
-letsencrypt --renew --webroot -w $WEBROOT --domain $DOMAIN --email $EMAIL --rsa-key-size $KEY_SIZE certonly
+if [ ! -e $WEBROOT/dh.pem ]; then
+  openssl dhparam -out $WEBROOT/dh.pem 2048 
+fi
+
+letsencrypt --renew-by-default --webroot -w $WEBROOT --domain $DOMAIN --email $EMAIL --rsa-key-size $KEY_SIZE certonly
 /etc/init.d/nginx restart
