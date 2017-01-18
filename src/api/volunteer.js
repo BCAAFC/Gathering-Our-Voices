d7486557b0d0aa0971e66009efe3f8a09b922522
Andrew Hobden <andrew@hoverbear.org>
2017-01-18 19:59:43 +0100

    Fix vokunteer csv

--- src/api/volunteer.js
+++ src/api/volunteer.js
@@ -97,19 +97,19 @@ module.exports = function (db, redis) {
             volunteer.notes = req.body.notes;
 
             Object.keys(req.body).filter(key => key.startsWith('day')).map(key => {
-              console.log(`isAdmin ${req.session.isAdmin} | Key ${req.body[key]}`);
+            //   console.log(`isAdmin ${req.session.isAdmin} | Key ${req.body[key]}`);
               if (req.body[key] === 'on') {
                 // They've just turned it on.
                 volunteer[key] = 'Available';
-                console.log(`Turning ${key} on.`);
+                // console.log(`Turning ${key} on.`);
               } else if (req.session.isAdmin && req.body[key]) {
                 // Admin is defining the task.
                 volunteer[key] = req.body[key];
-                console.log(`Setting ${key} to ${req.body[key]}.`);
+                // console.log(`Setting ${key} to ${req.body[key]}.`);
               } else {
                 // It's no longer defined.
                 volunteer[key] = null;
-                console.log(`Nulling ${key}`);
+                // console.log(`Nulling ${key}`);
               }
             });
 
@@ -285,24 +285,6 @@ module.exports = function (db, redis) {
                 volunteers[vol_num].applied = String(volunteers[vol_num].applied);
                 volunteers[vol_num].approved = String(volunteers[vol_num].approved);
                 volunteers[vol_num].followUp = String(volunteers[vol_num].followUp);
-                var schedule = volunteers[vol_num].schedule;
-                for (var i=0; i < schedule.length; i++) {
-                    var entry = schedule[i];
-                    var day = entry.day.split(",")[0];
-                    if (entry.morning) {
-                        volunteers[vol_num][day + "-morning-available"] = String(entry.morning.available);
-                        volunteers[vol_num][day + "-morning-scheduled"] = String(entry.morning.scheduled);
-                    }
-                    if (entry.afternoon) {
-                        volunteers[vol_num][day + "-afternoon-available"] = String(entry.afternoon.available);
-                        volunteers[vol_num][day + "-afternoon-scheduled"] = String(entry.afternoon.scheduled);
-                    }
-                    if (entry.evening) {
-                        volunteers[vol_num][day + "-evening-available"] = String(entry.evening.available);
-                        volunteers[vol_num][day + "-evening-scheduled"] = String(entry.evening.scheduled);
-                    }
-                }
-                delete volunteers[vol_num].schedule;
             }
 
             return new Promise(function (resolve, reject) {
